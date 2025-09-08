2384d09d0aeba6ccec47b9f56096a2a49bd2707f
Peter <peteryuli1204@gmail.com>
2025-09-07 19:44:33 -0700

    Vercel hosting

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,14 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Sunmarize!");
+});
+
 app.use("/api/completion", completionRouter);
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+if (process.env.NODE_ENV === "development") {
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+}
